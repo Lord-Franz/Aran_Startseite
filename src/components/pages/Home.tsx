347438a1891f4505dafc798bfd78e8199e2b5af6
Lord_Franz <123095870+Lord-Franz@users.noreply.github.com>
2025-04-17 14:24:54 +0200

    Integration eines Kalenders und einpflegung des Instagram Links

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -17,7 +17,7 @@ const Home = () => {
             </div>
             <div className={"h-[50vw] grid grid-flow-col grid-rows-3 grid-cols-5 pt-5 px-5"}>
                 <iframe className={"w-full h-full row-span-3 col-span-4"} src={"https://www.youtube.com/embed/Q3fSABkZf3E"} allow={"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"} allowFullScreen title={"Aran Trailer"}/>
-                <div className={""}>
+                <div>
                     <NamedPictureLink label={"Discord"} color={""} link={"https://discord.com/invite/rycTSt5Ghd"} src={"Discord_Icon.svg"} alt={"Discord Weiterleitung"}/>
                     <NamedPictureLink label={"Youtube"} color={""} link={"https://www.youtube.com/@land-von-aran9894"} src={"youtube_icon.svg"} alt={"Youtube Weiterleitung"}/>
                     <NamedPictureLink label={"Instagram"} color={""} link={"https://www.instagram.com/landvonaran?igsh=dGp1aTJqeGp6NTM="} src={"instagram_icon.svg"} alt={"Instagram Weiterleitung"}/>
