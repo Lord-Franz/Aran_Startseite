ae31a0006c768579be648668f8ef13b9ba18a31c
Lord_Franz <123095870+Lord-Franz@users.noreply.github.com>
2025-04-17 12:25:15 +0200

    Kleine Warning entfernt

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
 
             </div>
             <div className={"h-[50vw] grid grid-flow-col grid-rows-3 grid-cols-5 pt-5 px-5"}>
-                <iframe className={"w-full h-full row-span-3 col-span-4"} src={"https://www.youtube.com/embed/Q3fSABkZf3E"} frameBorder={"0"} allow={"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"} allowFullScreen title={"Aran Trailer"}/>
+                <iframe className={"w-full h-full row-span-3 col-span-4"} src={"https://www.youtube.com/embed/Q3fSABkZf3E"} allow={"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"} allowFullScreen title={"Aran Trailer"}/>
                 <div className={""}>
                     <NamedPictureLink label={"Discord"} color={""} link={"https://discord.com/invite/rycTSt5Ghd"} src={"Discord_Icon.svg"} alt={"Discord Logo"}/>
                     <NamedPictureLink label={"Youtube"} color={""} link={"https://www.youtube.com/@land-von-aran9894"} src={"youtube_icon.svg"} alt={"Youtube Logo"}/>
